ffcb6eeec1e0daaa56f5fbb8c16a9fbee81090a1
iriand <andrew.p.ben@gmail.com>
2018-09-27 23:25:47 +0300

    add coverage

---
+++ README.md
@@ -0,0 +1,3 @@
+#email-address-parser
+
+[![Coverage Status](https://coveralls.io/repos/github/iriand/node-email-address-parser/badge.svg?branch=master)](https://coveralls.io/github/iriand/node-email-address-parser?branch=master)
--- test/parser.test.js
+++ test/parser.test.js
@@ -5,11 +5,14 @@ const assert = require('assert');
 const EmailParser = require('../src/parser');
 
 describe('EmailParser().getDomainName', function() {
-  it('empty domain name in \'undefined\' email address', function() {
+  it('empty domain name from \'undefined\' email address', function() {
     assert.equal(new EmailParser(undefined).getDomainName(), '');
   });
-  it('empty domain name in \'null\' email address', function() {
+  it('empty domain name from \'null\' email address', function() {
     assert.equal(new EmailParser('').getDomainName(), '');
+  });
+  it('empty domain name from wrong email address value type', function() {
+    assert.equal(new EmailParser(123).getDomainName(), '');
   });
   it('empty domain name in empty email address', function() {
     const emailParser = new EmailParser('');
@@ -21,7 +24,7 @@ describe('EmailParser().getDomainName', function() {
   it('empty domain name of empty domain part email address', function() {
     assert.equal(new EmailParser('ben').getDomainName(), '');
   });
-  it('domain name of email address', function() {
+  it('domain name of normal email address', function() {
     assert.equal(new EmailParser('andrew@test.com').getDomainName(), 'test.com');
     assert.equal(
       new EmailParser('andrew.ben@test.com').getDomainName(),
@@ -32,16 +35,19 @@ describe('EmailParser().getDomainName', function() {
 });
 
 describe('EmailParser().getUserName', function() {
-  it('empty user name in \'undefined\' email address', function() {
+  it('empty user name from \'undefined\' email address', function() {
     const undefinedEmailAddress = undefined;
     const emailParser = new EmailParser(undefinedEmailAddress);
     assert.equal(emailParser.getUserName(), '');
   });
-  it('empty user name in \'null\' email address', function() {
+  it('empty user name from \'null\' email address', function() {
     const emailParser = new EmailParser('');
     assert.equal(emailParser.getUserName(), '');
   });
-  it('empty user name in empty email address', function() {
+  it('empty user name from wrong email address value type', function() {
+    assert.equal(new EmailParser(123).getUserName(), '');
+  });
+  it('empty user name from empty email address', function() {
     const emailParser = new EmailParser('');
     assert.equal(emailParser.getUserName(), '');
   });
@@ -49,7 +55,7 @@ describe('EmailParser().getUserName', function() {
     assert.equal(new EmailParser('andrew@').getUserName(), 'andrew');
     assert.equal(new EmailParser('ben').getUserName(), 'ben');
   });
-  it('user name of email address', function() {
+  it('user name of normal email address', function() {
     assert.equal(new EmailParser('andrew@test.com').getUserName(), 'andrew');
     assert.equal(new EmailParser('andrew.ben@test.com').getUserName(), 'andrew.ben');
     assert.equal(new EmailParser('a@b.c').getUserName(), 'a');
